86beb1dcd28777d662f903c792116acb2e5d10b0
Astroleander <astroleander@gmail.com>
2021-01-05 14:30:53 +0800

    [ fix ] rename typo

--- src/lab-scenario/router/index.js
+++ src/lab-scenario/router/index.js
@@ -1,14 +1,14 @@
 const routes = [];
 
-const name_praser_in_webpack_5 = (key) => key.split(/\//)[2]
-const path_praser_in_webpack_5 = (key) =>  `views/${key.split(/\//).splice(2).join('/')}`
+const name_parser_in_webpack_5 = (key) => key.split(/\//)[2]
+const path_parser_in_webpack_5 = (key) =>  `views/${key.split(/\//).splice(2).join('/')}`
 
 require.context('@/lab-scenario/views/', true, /^lab.*?index\.[j|t]sx?$/)
   .keys()
   .forEach(key => {
     routes.push({
-      name: name_praser_in_webpack_5(key),
-      path: path_praser_in_webpack_5(key),
+      name: name_parser_in_webpack_5(key),
+      path: path_parser_in_webpack_5(key),
     })
   })
 
