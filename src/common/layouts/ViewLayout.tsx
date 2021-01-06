f4d69d411a699e6a61086a0146b930479db5dbd1
Astroleander <astroleander@gmail.com>
2021-01-06 23:58:15 +0800

    [ add ] view layout with hook

--- src/common/layouts/ViewLayout.tsx
+++ src/common/layouts/ViewLayout.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
+
+const useLazyComponent = (location) => {
+  const [component, setComponent] = useState<React.FC|null>(null);
+  useLayoutEffect(() => {
+    import(`@/${location.pathname.substring(1)}${location.hash.substring(2)}`).then(m => {
+      setComponent(m.default);
+    })
+  }, [])
+  return component;
+}
 
 const ViewLayout = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  const Component = useLazyComponent(location);
+  if (Component) return Component;
+  else return <div>loading...</div>;
 }
 
 export default ViewLayout;
--- src/lab-scenario/views/player_maker/index.jsx
+++ src/lab-scenario/views/player_maker/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const View = () => {
   return (
     <div>
-      
+      Player Maker
     </div>
   );
 }
