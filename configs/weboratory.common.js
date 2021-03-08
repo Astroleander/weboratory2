20d5699e70c0ec268b12c11eec2297ba6b621e45
Astroleander <astroleander@gmail.com>
2021-03-08 11:40:52 +0800

    [ feat ] dark mode

--- configs/weboratory.common.js
+++ configs/weboratory.common.js
@@ -1,2 +1,5 @@
 import '@/common/styles/global.css';
 import '@/common/styles/router-list.less';
+
+import '@/common/styles/theme.less';
+document.querySelector('html').dataset.theme = 'theme-light';
