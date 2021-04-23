043860ddc24e57e7fa3e12817e4b72090b3f9758
Astroleander <astroleander@gmail.com>
2021-04-23 19:24:00 +0800

    [🍕addon] a hand writing promise

--- src/lab-framework/views/js/handwriting_promise/index.tsx
+++ src/lab-framework/views/js/handwriting_promise/index.tsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import ReactDOM from 'react-dom';
+import { APromise } from './promise';
 
+/** 有 BUG, 我总觉得 then 的新 promise 对象没有复用已有的微任务队列 */
 const Index = () => {
+  const [state, setState] = useState<(number | string)[]>(['init component']);
+  const ref = useRef<any>(0);
+  useEffect(() => {
+    setState(v => v.concat('outter start'));
+    const executor = (resolve, reject) => {
+      setState(v => v.concat('executor running'));
+      ref.current = setInterval(() => {
+        setState(v => {
+          const add = Number.isNaN(Number(v[v.length - 1])) ? 0 : Number(v[v.length - 1]) + 1;
+          return v.concat(add)
+        })
+        setState(v => {
+          if (v.length > 10) resolve('try end in then 1')
+          return v;
+        })
+      }, 100);
+    };
+    const Promise = APromise
+    let c = new Promise<string|number>(executor);
+    c.then((res) => {
+      setState(v => v.concat(res));
+      clearInterval(ref.current);
+      ref.current = null;
+    }).then(e => new Promise(r => {
+      setState(v => v.concat('then 2'))
+
+      new Promise(r => {
+        setState(v => v.concat('then 2.promise'));
+        r(1)
+      }).then(r => setState(v => v.concat('then 2.promise.then')))
+      r(1);
+      return new Promise(r => setState(v => v.concat('then 2.promise.then.return')))
+    })).then((r) => {
+      setState(v => v.concat('then 3.done'))
+    })
+    setState(v => v.concat('outter end'))
+  },[]);
+  
+  
   return (
     <>
       <h1>Promise with A+</h1>
+      <div>{ state.map(v => <div key={v}>{v}</div>) }</div>
     </>
   )
 }
---
+++ src/lab-framework/views/js/handwriting_promise/promise.ts
@@ -0,0 +1,104 @@
+enum STATUS {
+  PENDING   = 'PENDING',
+  FULFILLED = 'FULFILLED',
+  REJECTED  = 'REJECTED'
+}
+  
+class APromise<T=any> {
+  status: STATUS = STATUS.PENDING;
+  value: any = null;
+  reason: any = null;
+  onFulfulledCallbacks: any[] = [];
+  onRejectedCallbacks: any[] = [];
+  
+  constructor(executor: (resolve: (value: T | void) => void, reject: (reason?: any) => void) => void) {
+    const resolve = (val) => {
+      if (this.status === STATUS.PENDING) {
+        this.status = STATUS.FULFILLED;
+        this.value = val;
+        /** 一旦不是 fulfilled 会在上面就被挡住, 不会把整个队列跑空 */
+        this.onFulfulledCallbacks.forEach(next_resolve => next_resolve(this.value))
+      }
+    }
+    const reject = (res) => {
+      if (this.status === STATUS.PENDING) {
+        this.status = STATUS.REJECTED;
+        this.reason = res;
+        this.onRejectedCallbacks.forEach(next_reject => next_reject(this.reason))
+      }
+    }
+
+    try {
+      executor(resolve, reject);
+    } catch (err) {
+      reject(err);
+    }
+  }
+
+  then(
+    onFulfulled: ((value: T) => T | APromise<T> | void) ,
+    onRejected?: ((value: T) => T | APromise<T> | void) ,
+  ) {
+    switch (this.status) {
+      // 上一个任务还在执行中, 那么把当前任务加入执行队列
+      case STATUS.PENDING:
+        /** 只考虑 then 返回值可能是一个 function */
+        `
+        return new APromise<T>(() => {
+          this.onFulfulledCallbacks.push(/* 下一个任务 */ onFulfulled);
+          this.onRejectedCallbacks.push(/* 下一个任务: 失败的回调 */ onRejected);  
+        })
+        `
+        /** 考虑 then 返回值实际上可能是 ((value: T) => T | APromise<T>) | null | undefined */
+        return new APromise<T>((resolve, reject) => {
+          /** 下面实际是一个 executor 了, 在 PENDING 时直接将内容加入队列末尾 */
+          this.onFulfulledCallbacks.push(() => {
+            // 执行 then(f)
+            let then_result = onFulfulled(this.value);
+            then_result instanceof APromise
+              // 如果执行结果是 Promise 就再次构造它,把它加到队列末尾
+              ? then_result.then(resolve, reject)
+              // 否则说明这是一个简单值, 直接作为把当前 APromise 执行掉
+              : resolve(then_result)
+          });
+          this.onRejectedCallbacks.push(() => {
+            if (!onRejected) { reject(); return; }
+
+            let then_result = onRejected(this.reason);
+            then_result instanceof APromise
+              ? then_result.then(resolve)
+              : resolve(then_result)
+          })
+        })
+
+        /** FULFILLED & REJECTED 的情况实际上更简单, 只需要根据当前流直接执行即可, 不涉及缓存的任务队列 */
+        // 上一个任务已经执行完成
+        
+      case STATUS.FULFILLED:
+          return new APromise<T>((resolve, reject) => {
+            try {
+              let then_result = onFulfulled(this.value)
+              then_result instanceof APromise
+                ? then_result.then(resolve, reject)
+                : resolve(then_result)
+            } catch (err) {
+              reject(err)
+            }
+          });
+        case STATUS.REJECTED:
+          return new APromise<T>((resolve, reject) => {
+            try {
+              if (!onRejected) { reject(); return; }
+              let then_result = onRejected(this.value)
+              then_result instanceof APromise
+                ? then_result.then(resolve, reject)
+                : resolve(then_result)
+            } catch (err) {
+              reject(err)
+            }
+          });
+    }
+  }
+}
+
+export { APromise }
