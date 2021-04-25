dfbd0d693e240e679e1b14bd7fb471a7e9f06d5e
Astroleander <astroleander@gmail.com>
2021-04-25 17:53:37 +0800

    [🌟boost] support showing code & test case and results

---
+++ docs/framework.md
@@ -0,0 +1,3 @@
+# JS
+比较好的 A* Promise 实现
+https://www.chenhanpeng.com/handwriting-promise-accord-with-promise-a-plus-specification/
--- src/lab-framework/views/js/handwriting_promise/index.tsx
+++ src/lab-framework/views/js/handwriting_promise/index.tsx
@@ -1,16 +1,22 @@
 import React, { useState, useRef, useEffect } from 'react'
 import ReactDOM from 'react-dom';
 import { APromise } from './promise';
+import styles from './index.modules.less'
 
-/** 有 BUG, 我总觉得 then 的新 promise 对象没有复用已有的微任务队列 */
-const Index = () => {
-  const [state, setState] = useState<(number | string)[]>(['init component']);
-  const ref = useRef<any>(0);
+const CodePlaneOfClass: React.FC<{ source: Object }> = ({ source }) => {
+  return <pre className={styles['code-source-plane']}>
+    {source.toString()}
+  </pre>
+}
+
+const CasePlane: React.FC<{}> = ({}) => {
+  const [state, setState] = useState<(number | string)[]>(['init component state']);
+  const timer = useRef<any>(0);
+  const test_format = useRef<any>(0);
   useEffect(() => {
-    setState(v => v.concat('outter start'));
     const executor = (resolve, reject) => {
-      setState(v => v.concat('executor running'));
-      ref.current = setInterval(() => {
+      setState(v => v.concat('[executor] running'));
+      timer.current = setInterval(() => {
         setState(v => {
           const add = Number.isNaN(Number(v[v.length - 1])) ? 0 : Number(v[v.length - 1]) + 1;
           return v.concat(add)
@@ -21,37 +27,51 @@ const Index = () => {
         })
       }, 100);
     };
-    const Promise = APromise
-    let c = new Promise<string|number>(executor);
-    c.then((res) => {
-      setState(v => v.concat(res));
-      clearInterval(ref.current);
-      ref.current = null;
-    }).then(e => new Promise(r => {
-      setState(v => v.concat('then 2'))
-
-      new Promise(r => {
-        setState(v => v.concat('then 2.promise'));
-        r(1)
-      }).then(r => setState(v => v.concat('then 2.promise.then')))
-      r(1);
-      return new Promise(r => setState(v => v.concat('then 2.promise.then.return')))
-    })).then((r) => {
-      setState(v => v.concat('then 3.done'))
-    })
-    setState(v => v.concat('outter end'))
-  },[]);
+    const test = () => {
+      setState(v => v.concat('test macro task start'));
+      const Promise = APromise;
+      // generator 0 to 6
+      let c = new Promise<string|number>(executor);
+      c.then((res) => {
+        setState(v => v.concat(res));
+        clearInterval(timer.current);
+        timer.current = null;
+      }).then(e => new Promise(r => {
+        setState(v => v.concat('then 2'))
   
+        new Promise(r => {
+          setState(v => v.concat('then 2.promise'));
+          r(1)
+        }).then(r => setState(v => v.concat('then 2.promise.then')))
+        r(1);
+        return new Promise(r => setState(v => v.concat('then 2.promise.then.return')))
+      })).then((r) => {
+        setState(v => v.concat('then 3.done'))
+      })
+      setState(v => v.concat('test macro task end'))
+    }
+    test();
+    test_format.current = test.toString()
+  }, []);
+  return (<>
+    <pre className={styles['test-case-plane']}>{test_format.current}</pre>
+    <div className={styles['showing-case-plane']}>{state.map(v => <div key={v}>{v}</div>)}</div>
+  </>);
+}
+
+const Index = () => {
   
   return (
     <>
       <h1>Promise with A+</h1>
-      <div>{ state.map(v => <div key={v}>{v}</div>) }</div>
+      <CodePlaneOfClass source={APromise}></CodePlaneOfClass>
+      <CasePlane></CasePlane>
     </>
   )
 }
 
 const mount = document.createElement('div');
+mount.className = styles['sandbox-instance'];
 
 export default [
   () => {
