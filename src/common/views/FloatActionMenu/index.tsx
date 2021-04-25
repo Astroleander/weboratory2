bee8ec34f893cbb15063335b0661b0f52dd55efa
Astroleander <astroleander@gmail.com>
2021-04-25 18:09:38 +0800

    [🌈beautify] fab can be hidden now

--- src/common/views/FloatActionMenu/index.tsx
+++ src/common/views/FloatActionMenu/index.tsx
@@ -7,13 +7,19 @@ import FloatActionButton from './floating_button'
 import styles from './floating_button.modules.less'
 import { toggle as toggleDarkMode } from '@/common/modules/darkmode';
 
+const wrapper = document.querySelector('#float');
+const anchor = document.createElement('menuitem');
+anchor.className = styles['floating-menu'];
+wrapper?.appendChild(anchor);
+
 const FloatActionMenu = () => {
   return (
     <>
       <FloatActionButton onClick={() => fab.switchDarkMode()}><Dark/></FloatActionButton>
       <FloatActionButton onClick={() => fab.backToHome()}><Home /></FloatActionButton>
       <FloatActionButton onClick={() => fab.backToPrev()}><Back /></FloatActionButton>
-      <FloatActionButton></FloatActionButton>
+      <FloatActionButton onClick={() => fab.hiddenFAB()}>H/S</FloatActionButton>
+      {/* <FloatActionButton></FloatActionButton> */}
     </>
   );
 }
@@ -27,13 +33,10 @@ const fab = {
   },
   backToPrev() {
     history.go(-1);
+  },
+  hiddenFAB() {
+    anchor.classList.toggle(styles['hidden'])
   }
 }
-
-const wrapper = document.querySelector('#float');
-const anchor = document.createElement('menuitem');
-anchor.className = styles['floating-menu'];
-
-wrapper?.appendChild(anchor);
 
 export default ReactDOM.render(<FloatActionMenu />, anchor);
