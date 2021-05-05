bdc564afb14f9d0c18eba7ade631606a2f71c7cd
Astroleander <astroleander@gmail.com>
2021-05-06 02:09:45 +0800

    [🌟upgrade] support complete hidden for fab menu

--- src/common/views/FloatActionMenu/index.tsx
+++ src/common/views/FloatActionMenu/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Dark from './icon_dark';
 import Home from './icon_home';
@@ -8,18 +8,41 @@ import styles from './floating_button.modules.less'
 import { toggle as toggleDarkMode } from '@/common/modules/darkmode';
 
 const wrapper = document.querySelector('#float');
-const anchor = document.createElement('menuitem');
+const anchor = document.createElement('section');
 anchor.className = styles['floating-menu'];
+
 wrapper?.appendChild(anchor);
 
-const FloatActionMenu = () => {
+const Wrapper = () => {
   return (
+    <>
+      <FloatActionMenu />
+      <ToggleFloatActionButton />
+    </>
+  )
+}
+
+const Menulize = (Component, hidden = false) => {
+  const clist = hidden ? styles['hidden'] : ''
+  return (
+    <menu className={clist}>
+      {Component}
+    </menu>
+  )
+}
+
+const ToggleFloatActionButton = () => Menulize(
+    <FloatActionButton onClick={() => fab.hiddenFAB()}>H/S</FloatActionButton>, true
+  );
+
+
+const FloatActionMenu = () => {
+  return Menulize(
     <>
       <FloatActionButton onClick={() => fab.switchDarkMode()}><Dark/></FloatActionButton>
       <FloatActionButton onClick={() => fab.backToHome()}><Home /></FloatActionButton>
       <FloatActionButton onClick={() => fab.backToPrev()}><Back /></FloatActionButton>
       <FloatActionButton onClick={() => fab.hiddenFAB()}>H/S</FloatActionButton>
-      {/* <FloatActionButton></FloatActionButton> */}
     </>
   );
 }
@@ -35,8 +58,10 @@ const fab = {
     history.go(-1);
   },
   hiddenFAB() {
-    anchor.classList.toggle(styles['hidden'])
+    for (const ele of anchor.children) {
+      ele.classList.toggle(styles['hidden'])
+    } 
   }
 }
 
-export default ReactDOM.render(<FloatActionMenu />, anchor);
+export default ReactDOM.render(<Wrapper />, anchor);
